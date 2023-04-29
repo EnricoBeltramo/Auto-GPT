7aa1c3d1ef7f5525a8c4911d37c5deee722564e0
Enrico Beltramo <enrico.beltramo@inventio.ai>
2023-04-29 12:09:34 +0200

    Minor fixes

---
+++ createenv.cmd
@@ -0,0 +1,9 @@
+call conda remove -n chatgpt-env-dev -y --all
+call conda create -n chatgpt-env-dev -y python=3.8
+call conda activate chatgpt-env-dev
+
+REM Base packages:
+call pip install -r requirements.txt
+
+
+call conda deactivate
